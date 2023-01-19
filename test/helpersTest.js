8082a4eac56a55df068e8a5acfa301be9e013dee
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-01-18 18:56:23 -0800

    eslint tool

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,32 +1,32 @@
 const { assert } = require('chai');
 
 const getUserByEmail = require('../helper');
-console.log(getUserByEmail)
+console.log(getUserByEmail);
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     // Write your assert statement here
-    assert.deepEqual(user.id, expectedUserID, "are the same emails")
+    assert.deepEqual(user.id, expectedUserID, "are the same emails");
   });
 
   it('should return a undefined because the email is not in the database', function() {
-    const user = getUserByEmail("user3@example.com", testUsers)
+    const user = getUserByEmail("user3@example.com", testUsers);
     const expectedUserID = undefined;
     // Write your assert statement here
-    assert.deepEqual(user.id, expectedUserID, "email is not in the database")
+    assert.deepEqual(user.id, expectedUserID, "email is not in the database");
   });
 });
