01d1d8bed9d27a4a2afeb1c5eee2af4d3a18fc7c
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-01-18 18:55:29 -0800

    eslint tool

--- helper.js
+++ helper.js
@@ -1,14 +1,11 @@
 const getUserByEmail = (email, database) => {
-  // const userID = req.cookies["user_id"];
-  // const user = users[userID];
-  // const currentEmail = user.email;
-  // let message = null;
-  for( let user in database ){
-    if(database[user].email === email){
-      return database[user]
+  
+  for (let user in database) {
+    if (database[user].email === email) {
+      return database[user];
     }
   }
   return {};
-}
+};
 
 module.exports = getUserByEmail;
